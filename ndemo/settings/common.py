ae0015aa02bde223cc117090e7c9452fe1cbeb20
ndemo-richard <ndemorich@gmail.com>
2021-01-28 17:06:57 +0300

    update

--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
