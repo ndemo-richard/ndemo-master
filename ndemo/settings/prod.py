70c02b09d86126fca7ed984dd39ab6d876a1d98d
ndemo-richard <ndemorich@gmail.com>
2021-01-28 21:11:14 +0300

    update

--- ndemo/settings/prod.py
+++ ndemo/settings/prod.py
@@ -3,7 +3,7 @@ from ndemo.settings.common import *
 SECRET_KEY = 'l6ypqe5be68n@b40+(9yo0!0e9j*o-8(+2)^^mg173*cy=tybn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
