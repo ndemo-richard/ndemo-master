16e400adb5335cfe0e3c7701095e930b60f1454e
ndemo-richard <ndemorich@gmail.com>
2021-01-28 19:18:57 +0300

    update

--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
@@ -129,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT =os.path.join( BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join( BASE_DIR, 'staticfiles')
 STATICFILES_DIRS =[ os.path.join(BASE_DIR,'static'),]
 
 STATICFILES_FINDERS = (
