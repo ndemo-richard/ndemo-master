2ea383c5a4f0970b128ba64252bad89767f40a58
richard ndemo <ndemorich@gmail.com>
2021-01-29 02:12:03 +0300

    update

--- ndemo/settings/prod.py
+++ ndemo/settings/prod.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['*']
 
 
 DATABASES = {
-    '"default": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
