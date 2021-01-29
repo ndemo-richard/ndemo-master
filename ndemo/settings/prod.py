2481bc1375e705a065430bb08bcbc39bc34edeac
richard ndemo <ndemorich@gmail.com>
2021-01-29 12:40:14 +0300

    update

--- ndemo/settings/prod.py
+++ ndemo/settings/prod.py
@@ -5,6 +5,6 @@ SECRET_KEY = 'l6ypqe5be68n@b40+(9yo0!0e9j*o-8(+2)^^mg173*cy=tybn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://ndemo-master.herokuapp.com/','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','https://ndemo-master.herokuapp.com/','0.0.0.0','*']
 
 
