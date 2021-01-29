f3ffb5241f426c1b2445dde72c41eeb27c96a6e5
richard ndemo <ndemorich@gmail.com>
2021-01-29 08:36:05 +0300

    update

--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -91,6 +91,11 @@ WSGI_APPLICATION = 'ndemo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
