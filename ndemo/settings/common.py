afa834176dc1e67b5236a989e660e9948576bf57
richard ndemo <ndemorich@gmail.com>
2021-01-29 14:04:36 +0300

    update

--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 #import dj_database_url
-import django_heroku
+#import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -161,6 +161,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 
