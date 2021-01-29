81b9fb7a4ddcfa11f402482ff7e80fcda73ef718
richard ndemo <ndemorich@gmail.com>
2021-01-29 17:57:59 +0300

    update

--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -14,8 +14,9 @@ from pathlib import Path
 
 import os
 #import dj_database_url
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')$c0fsmsqvl^z+^en7)hd9jnyx_+!koj%^8h4jkho=6e5#awpw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -145,22 +146,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATIC_ROOT =os.path.join( BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder'
 )
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 
--- ndemo/settings/prod.py
+++ ndemo/settings/prod.py
@@ -3,8 +3,8 @@ from ndemo.settings.common import *
 SECRET_KEY = 'l6ypqe5be68n@b40+(9yo0!0e9j*o-8(+2)^^mg173*cy=tybn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'FALSE'
 
-ALLOWED_HOSTS = ['127.0.0.1','https://ndemo-master.herokuapp.com/','0.0.0.0','*']
+ALLOWED_HOSTS = ['*','127.0.0.1','https://ndemo-master.herokuapp.com/','0.0.0.0']
 
 
