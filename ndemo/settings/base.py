c6b8d031bd1e5c660948bff3818f65a3cb50d6ce
richard ndemo <ndemorich@gmail.com>
2021-01-29 08:31:11 +0300

    update

--- ndemo/settings/base.py
+++ ndemo/settings/base.py
@@ -7,9 +7,3 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
--- ndemo/settings/prod.py
+++ ndemo/settings/prod.py
@@ -8,12 +8,3 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
