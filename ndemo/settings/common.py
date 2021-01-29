48a8bbc32e7e4db5288f0c8f92e5c56150efc8cc
richard ndemo <ndemorich@gmail.com>
2021-01-29 09:48:35 +0300

    update

--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -93,12 +93,12 @@ WSGI_APPLICATION = 'ndemo.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.sqlite3',
-        "NAME": os.path.join(BASE_DIR,'db.sqlite3'),
+        "ENGINE": 'django.db.backends.postgresql',
+  
         }
         }
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
