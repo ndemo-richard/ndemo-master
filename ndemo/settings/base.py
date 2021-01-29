37f1154ea13a114fdc6ee53270fd4269f86b0eb0
richard ndemo <ndemorich@gmail.com>
2021-01-29 09:32:14 +0300

    first

--- ndemo/settings/base.py
+++ ndemo/settings/base.py
@@ -5,12 +5,3 @@ SECRET_KEY = 'l6ypqe5be68n@b40+(9yo0!0e9j*o-8(+2)^^mg173*cy=tybn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ndemo_db',
-        'USER': 'root',
-        'PASSWORD': 'ndemo',
-    }
-}
--- ndemo/settings/common.py
+++ ndemo/settings/common.py
@@ -93,19 +93,13 @@ WSGI_APPLICATION = 'ndemo.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-    }
-}
+        "ENGINE": 'django.db.backends.sqlite3',
+        "NAME": os.path.join(BASE_DIR,'db.sqlite3'),
+        }
+        }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-# try to load local_settings.py if it exists
-'''
-try:
-  from base import *
-except Exception as e:
-  pass
-'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -157,3 +151,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 django_heroku.settings(locals())
+
